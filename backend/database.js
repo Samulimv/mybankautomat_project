9543fc2b36d7d6d7f1b7e711f9b54a2f064e162f
Tuomas Tavi <tuisi.tavi@gmail.com>
2024-03-29 15:41:57 +0200

    backend

---
+++ backend/database.js
@@ -0,0 +1,8 @@
+const mysql = require('../backend/node_modules/mysql2');
+const connection = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'bankuser',
+  password: 'bankpass',
+  database: 'bank'
+});
+module.exports = connection;
