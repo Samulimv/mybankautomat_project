df6899b4c07b71ed49fb0b8aad1b8632a7f72379
Samulimv <t3vasa03@students.oamk.fi>
2024-03-29 19:21:40 +0200

    crud operaatiot ei vielä toimi

--- backend/models/account_model.js
+++ backend/models/account_model.js
@@ -2,10 +2,27 @@ const db=require('../database');
 
 const account={
     getAllAccount(callback){
-        return db.query("SELECT * FROM account", callback);
+        return db.query('SELECT * FROM account', callback);
+ 
+    },
+    getById: function(id, callback){
+        return db.query('SELECT * FROM account WHERE id_account=?', [id], callback);
+   
+    },
+    add: function(account, callback){
+        return db.query('INSERT INTO account (accountNumber,balance) values(?,?)', [account.accountNumber,account.balance], callback);
+
+    },
+    delete: function(id, callback){
+        return db.query('DELETE FROM account WHERE id_account=?', [id], callback);
+        
+    },
+    update: function(id, account, callback){
+        return db.query('UPDATE account SET accountNumber=?,balance=? WHERE id_account=?', [account.accountNumber, account.balance, id], callback);
 
     }
 
-}
+};
 
 module.exports=account;
+
--- backend/routes/account.js
+++ backend/routes/account.js
@@ -5,16 +5,62 @@ const account=require('../models/account_model');
 router.get('/',function(request, response){
     account.getAllAccount(function(err, result){
         if(err){
-            response.send(err);
+            response.json(err);
         }
         else{
-            response.send(result);
-            console.log(result);
+            response.json(result);
         }
+    })
+});
 
+router.get('/:id',
+function(request, response){
+    account.getById(request.params.id, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else{
+            response.json(result);
+        }
+    })
+});
+
+router.post('/',
+function(request,response) {
+    account.add(request.body, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else{
+            response.json(result);
+        }
     });
+});
 
+router.delete('/:id',
+function(request, response){
+    account.delete(request.params.id, function(err,result){
+        if(err){
+            response.json(err);
+        }
+        else{
+            response.json(result);
+        }
+    });
+});
 
+router.put('/:id',
+function(request, response){
+    account.update(request.params.id, request.body, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else{
+           response.json(result);
+        }
+    });
 });
 
 module.exports=router;
+
+
