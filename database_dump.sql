d54cdd8fe73e9929251c0b93c699fcaa286eac77
Simo Unhola <unhola.simo@gmail.com>
2024-04-02 12:35:03 +0300

    ER kaavio

--- database_dump.sql
+++ database_dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bank` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bank`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: bank
@@ -154,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-02 11:29:28
+-- Dump completed on 2024-04-02 12:21:18
