96bc7c487b5fcca4c4165b171b1a2478187cafa7
Tuomas Tavi <tuisi.tavi@gmail.com>
2024-04-16 12:05:00 +0300

    sql dump updated

--- database_dump.sql
+++ database_dump.sql
@@ -149,6 +149,70 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'Teppo',' Testi'),(2,'Aino',' Joki'),(3,'Pekka','Rinne');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'bank'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `credit_transfer` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `credit_transfer`(IN first_id INT ,IN credit_limit double, IN amount dec(10,2), IN transactionType varchar(25))
+BEGIN
+  DECLARE test1 INT DEFAULT 0;
+  START TRANSACTION;
+  UPDATE account SET balance=balance-amount WHERE id_account=first_id AND balance + credit_limit >= amount;
+  SET test1=ROW_COUNT();
+
+    IF (test1 > 0 ) THEN
+      COMMIT;
+      INSERT INTO transactions(id_account,amount,transactionType, transactionDate) VALUES(first_id,amount,transactiontype, NOW());
+     
+    ELSE
+      ROLLBACK;
+  END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `debit_transfer` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `debit_transfer`(IN first_id INT , IN amount dec(10,2), IN transactionType varchar(25) )
+BEGIN
+  DECLARE test1,test2 INT DEFAULT 0;
+  START TRANSACTION;
+  UPDATE account SET balance=balance-amount WHERE id_account=first_id AND balance >= amount;
+  SET test1=ROW_COUNT();
+  
+    IF (test1 > 0 ) THEN   
+      COMMIT;    
+      INSERT INTO transactions(id_account,amount, transactionType, transactionDate) VALUES(first_id,amount,transactiontype, NOW());
+      
+    ELSE
+      ROLLBACK;
+  END IF;
+  END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -159,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-12 10:42:59
+-- Dump completed on 2024-04-16 12:04:02
