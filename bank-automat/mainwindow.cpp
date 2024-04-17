4836f479e577e24c24847b74db34b37f0e08c812
Samulimv <156782814+Samulimv@users.noreply.github.com>
2024-04-17 13:11:15 +0300

    Revert "Samuli"

--- bank-automat/mainwindow.cpp
+++ bank-automat/mainwindow.cpp
@@ -1,24 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "pin.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    pinDialog = new pin(this);
-
-    connect(ui->pinDialog, &QPushButton::clicked, this, &MainWindow::openPinDialog);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void MainWindow::openPinDialog()
-{
-    pinDialog->show();
 }
