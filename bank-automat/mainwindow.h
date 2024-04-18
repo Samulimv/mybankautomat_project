0cc57f9d14d13509f62be4dcb4d10e2f1fa15386
Simo Unhola <simbabve99@gmail.com>
2024-04-18 12:53:46 +0300

    saa kokkeilla

--- bank-automat/mainwindow.h
+++ bank-automat/mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "QLabel.h"
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
+#include "pin.h"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,15 +16,35 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+class Rfidreader;  // Forward declaration
+class pin;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void showTransactions();
+    void showPinDialog();
+    void sendDataToServer(const QJsonObject &data);
+
+
+private slots:
+    void displayTagId(const QString &tagId);
+    void fetchPasswordFromServer(const QString &tagId);
+    void handleNetworkReply(QNetworkReply* reply);
+    void handlePinVerified(bool success);
+    void openPin();
 
 private:
     Ui::MainWindow *ui;
+    Rfidreader * reader;
+    QLabel *statusLabel;
+    QNetworkAccessManager *networkManager;
+    pin * pinDialog;
+
 };
-#endif // MAINWINDOW_H
+#endif
+
